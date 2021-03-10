e751f87278c7655bcb21d00cf74edeaae1e81df5
Omar Magdy <contact.parmag@gmail.com>
2021-03-10 12:30:37 +0200

    views.py:add:articles

--- simpleApp/articles/views.py
+++ simpleApp/articles/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 def articles_list_page(request):
-	return render(request, "articles/articles_list.html")
+	articles = Article.objects.all().order_by("id")
+	return render(request, "articles/articles_list.html",
+		{"articles":articles})
